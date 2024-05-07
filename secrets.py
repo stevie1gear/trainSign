114d61ca54452172789c2a068ead23c0745b55f9
Steven Maloney <smaloney@gatech.edu>
2024-05-07 15:32:04 -0600

    initial commit

---
+++ secrets.py
@@ -0,0 +1,10 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : '',
+    'password' : '',
+    'timezone' : "America/New_York",
+    'aio_username' : "",
+    'aio_key' : ""
+    }
